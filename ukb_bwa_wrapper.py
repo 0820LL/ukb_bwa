021df01a6fe0a652c65d168005de2c9a59624dcf
lilin <lilin@ubuntu>
2024-01-26 12:00:44 +0800

    根据config.json的格式做了修改

--- ukb_bwa_wrapper.py
+++ ukb_bwa_wrapper.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import json
 import time
+import logging
 
 def send_json_message(analysis_path:str, send_message_script: str, message: dict, step_file_name:str) -> None:
     os.system('bash {} \'{}\' 1>>final_message.log 2>&1'.format(send_message_script, json.dumps(message, ensure_ascii=False, indent=2)))
@@ -16,13 +17,11 @@ def steward(config_file_path:str, ukb_bwa_path:str, send_message_script:str) -> 
     # get paramters from the config.json
     with open(config_file_path, 'r') as config_f:
         config_d = json.load(config_f)
-    task_id = config_d['taskId']
-    analysis_record_id = config_d['analysisRecordId']
     params_d = {
-        'fastq_1':config_d['fastq_1'],
-        'fastq_2':config_d['fastq_2'],
-        'bwa_index':config_d['bwa_index'],
-        'threads_num':config_d['threads_num']
+        'fastq_1'    : config_d['ukbParams']['fastq_1'],
+        'fastq_2'    : config_d['ukbParams']['fastq_2'],
+        'bwa_index'  : config_d['ukbParams']['bwa_index'],
+        'threads_num': config_d['ukbParams']['threads_num']
     }
     # make the params.json file
     params_file_path = '{}/params.json'.format(analysis_path)
@@ -30,30 +29,30 @@ def steward(config_file_path:str, ukb_bwa_path:str, send_message_script:str) -> 
         json.dump(params_d, params_f, ensure_ascii=False, indent=4)
     ukb_bwa_command = 'nextflow run -offline -profile singularity -bg -params-file {} {} >> run_log.txt'.format(params_file_path, ukb_bwa_path)
     return_value = os.system(ukb_bwa_command)
-    with open('ukb_bwa_command.txt', 'w') as command_f:
-        command_f.write(ukb_bwa_command + '\n')
-        command_f.write('return value:{}\n'.format(str(return_value)))
+    logging.info(ukb_bwa_command)
+    logging.info('return value:{}\n'.format(str(return_value)))
     time.sleep(10)
     # send the result files 
     feedback_dict = {
-        'tTaskId': task_id,
-        'analysisRecordId': analysis_record_id,
-        'pipelineName': 'ukb_bwa',
+        'uuid'          : config_d['uuid'],
+        'ukbId'         : config_d['ukbId'],
+        'ukbToolsCode'  : config_d['ukbToolsCode'],
+        'ukbToolName'   : config_d['ukbToolName'],
         'analysisStatus': '',
-        'startDate': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
-        'endDate': '',
-        'error': 0,
-        'taskName': 'Step'
+        'startDate'     : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+        'endDate'       : '',
+        'error'         : 0,
+        'taskName'      : 'Step'
     }
     if return_value == 0 and os.path.exists('{}/results'.format(analysis_path)):
         feedback_dict['analysisStatus'] = '分析开始'
-        feedback_dict['endDate'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        feedback_dict['error'] = 0
+        feedback_dict['endDate']        = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        feedback_dict['error']          = 0
         send_json_message(analysis_path, send_message_script, feedback_dict, 'start.json')
     else:
         feedback_dict['analysisStatus'] = '分析开始'
-        feedback_dict['endDate'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        feedback_dict['error'] = 1
+        feedback_dict['endDate']        = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        feedback_dict['error']          = 1
         send_json_message(analysis_path, send_message_script, feedback_dict, 'start.json')
     flag = 0
     while True:
@@ -69,10 +68,10 @@ def steward(config_file_path:str, ukb_bwa_path:str, send_message_script:str) -> 
                 break
         time.sleep(5)
     feedback_dict['analysisStatus'] = '分析结果'
-    feedback_dict['endDate'] = ''
-    feedback_dict['taskName'] = 'Result'
-    feedback_dict['error'] = 0
-    feedback_dict['data'] = []
+    feedback_dict['endDate']        = ''
+    feedback_dict['taskName']       = 'Result'
+    feedback_dict['error']          = 0
+    feedback_dict['data']           = []
     flag = 0
     while True:
         with open(execution_trace_file, encoding = 'UTF-8') as trace_f:
@@ -85,20 +84,20 @@ def steward(config_file_path:str, ukb_bwa_path:str, send_message_script:str) -> 
                             if file.endswith('sam'):
                                 bwa_res_sam = '{}/results/{}'.format(analysis_path, file)
                         report_download_dict = {
-                            'sort': 1, 
-                            'title': '分析结果文件', 
-                            'text': [
+                            'sort' : 1,
+                            'title': '分析结果文件',
+                            'text' : [
                                 {
-                                    'sort':1,
-                                    'title': '分析结果sam文件',
-                                    'content': '分析结果sam文件：#&{}'.format(bwa_res_sam), 
-                                    'postDes': '', 
-                                    'memo': '', 
-                                    'preDes': ''
+                                    'sort'   : 1,
+                                    'title'  : '分析结果sam文件',
+                                    'content': '分析结果sam文件：#&{}'.format(bwa_res_sam),
+                                    'postDes': '',
+                                    'memo'   : '',
+                                    'preDes' : ''
                                 }
                             ], 
-                            'memo': '', 
-                            'subtitle1': '', 
+                            'memo'     : '',
+                            'subtitle1': '',
                             'subtitle2': ''
                         }
                         feedback_dict['data'].append(report_download_dict)
@@ -115,6 +114,7 @@ def steward(config_file_path:str, ukb_bwa_path:str, send_message_script:str) -> 
 
 
 def main() -> None:
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfp', required=True, help='the full path of the config.json')
     parser.add_argument('--ukb_bwa_path', required=True, help='the full path of the ukb_bwa')
@@ -124,9 +124,12 @@ def main() -> None:
     if os.path.isabs(config_file_path):
         pass
     else:
-        config_file_path = os.path.abspath(config_file_path)
+        config_file_path = os.path.abspath(os.path.basename(config_file_path))
     ukb_bwa_path = args.ukb_bwa_path
     send_message_script = args.send_message_script
+    # logging
+    log_file = '{}/ukb_bwa.log'.format(os.path.dirname(config_file_path))
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     steward(config_file_path, ukb_bwa_path, send_message_script)
 
 
